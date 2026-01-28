998643d79893fd43f6eb2fcabb46efd1eb4db6a2
Hina <hinakumari.725394@gmail.com>
2026-01-28 21:35:21 +0530

    Update calculator.java
    Added a simple calculator that performs basic arithmetic operations based on user input.

--- calculator.java
+++ calculator.java
@@ -1,1 +1,42 @@
+import java.util.*;
+public class calculator {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        System.out.println("enter first number");
+        Double a = Sc.nextDouble();
+        System.out.println("enter operator");
+        char operator = Sc.next().charAt(0);
+        System.out.println("enter second number");
+        Double b = Sc.nextDouble();
+        Double result;
+
+        switch (operator) {
+            case '+': result = a+b;
+                System.out.println("result :"+ result);
+                break;
+            case '-': result = a-b;
+            System.out.println("result :"+ result);
+            break;
+            case '*' : result = a*b;
+            System.out.println("result :"+ result);
+            case '/' :
+            if(b!=0){
+                result = a/b;
+                System.out.println("result :"+ result);
+            }else{
+               System.out.println("error");
+            }
+            break;
+        
+            default: System.out.println("invalid operator please enter valid operator");
+                
+        }
+
+
+    }
+    
+}
+
+
+
 
