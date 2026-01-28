a62177384ea50aeca06bcb8b9c3ca4501ebaa89b
Hina <hinakumari.725394@gmail.com>
2026-01-28 21:37:28 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -1,1 +1,104 @@
+import java.util.*;
+
+public class pattern {
+    public static void main(String[] args) {
+        System.out.println("enter the value of N ");
+        Scanner sc = new Scanner(System .in);
+        int n = sc.nextInt();
+        // int m = sc.nextInt();
+        // int number = 1;
+        
+        // shapes printin logic 
+        //  solid square shape
+        // for(int i=1; i<=n; i++){
+        //     for(int j=1; j<=m; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println("*");
+        // }
+
+        // hollo squre shape
+        // for(int i=1; i<=n; i++){
+        //     for(int j=1; j<=m; j++){
+        //         if(i == 1 || j == 1 || i == n || j == m){
+        //             System.out.print("*");
+        //         }else{
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+        // pyramid pattern
+        // for(int i=1; i<=n; i++){
+        //     for(int j=1; j<=i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+         // inverse pyramid pattern
+        // for(int i=n; i>=1; i--){
+        //     for(int j=1; j<=i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        //Rotate pyramid
+        // for(int i=1; i<=n; i++){
+        //     for(int j=1; j<=n-i; j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1; j<=i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // Number Pyramid
+        // for(int i=1; i<=n; i++){
+        //     for(int j=1; j<=i; j++){
+        //         System.out.print(j+" ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // Inverse number pyramid
+        // for(int i=1; i<=n; i++){
+        //     for(int j=1; j<=n-i+1; j++){
+        //         System.out.print(j+" ");
+        //     }
+        //     System.out.println();
+        // }
+        
+        //number increasing pyramid
+        // for(int i=1; i<=n; i++){
+        //     for(int j=1; j<=i; j++){
+        //         System.out.print(number+" ");
+        //         number++;
+        //     }
+        //     System.out.println();
+        // }
+
+        //0-1 Tringle
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                int sum = i+j;
+                if(sum % 2 == 0){
+                    System.out.print("1 ");
+                }else{
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+    
+}
+
+
+
+
 
